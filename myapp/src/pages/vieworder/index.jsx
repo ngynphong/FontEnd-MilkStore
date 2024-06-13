d06c801431be49de9bbfb244e3d89756396cbc5e
trung-trun <trungncse170145@fpt.edu.vn>
2024-06-13 13:51:41 +0700

    trung

--- myapp/src/pages/vieworder/index.jsx
+++ myapp/src/pages/vieworder/index.jsx
@@ -3,6 +3,49 @@
 import "./index.scss"
 
 function Vieworder() {
+    const columns = [
+        {
+          dataIndex: 'Code',
+          title: 'Code',
+          key: 'Code',
+        },
+        {
+          dataIndex: 'name',
+          title: 'Full Name',
+          key: 'name',
+        },
+        {
+            dataIndex: 'date',
+            title: 'Ngày mua',
+            key: 'date',
+        },
+        {
+          dataIndex: 'number',
+          title: 'Phone number',
+          key: 'number',
+        },
+        {
+          dataIndex: 'Address',
+          title: 'Address',
+          key: 'Address',
+        //   render: (id) => (
+        //     <Popconfirm
+        //       title="Delete the task"
+        //       description="Are you sure to delete this task?"
+        //       onConfirm={() => handleDeleteCustomer(id)}
+        //       okText="Yes"
+        //       cancelText="No"
+        //     >
+        //       <Button type='primary' danger>Delete</Button>
+        //     </Popconfirm>
+        //   )
+        },
+        {
+            dataIndex: 'money',
+            title: 'Tổng tiền',
+            key: 'money',
+          },
+      ];
     return (
         <div className="vieworder-list">
 
