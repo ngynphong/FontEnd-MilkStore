a2a919caf27531ecb9bc2fe06d5596d108d92180
trung-trun <trungncse170145@fpt.edu.vn>
2024-06-15 01:49:18 +0700

    trung

--- myapp/src/components/header/index.jsx
+++ myapp/src/components/header/index.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import { SearchOutlined, UserOutlined, ShoppingCartOutlined,CloseOutlined } from '@ant-design/icons';
+import { SearchOutlined, UserOutlined, ShoppingCartOutlined, CloseOutlined } from '@ant-design/icons';
 import "./index.scss";
 import { useState } from "react";
 function Header() {
 
-    const [isOpenSearch, setIsOpenSearch] =  useState(false);
+    const [isOpenSearch, setIsOpenSearch] = useState(false);
 
     return (
         <header className="header">
@@ -30,37 +30,37 @@ function Header() {
                             <Link to="/aboutme">Về chúng tôi</Link>
                         </li>
 
-                        {/* <li>
-                            <Link to="/milk-management">Quản lý sữa</Link>
-                        </li> */}
+                        <li>
+                            <Link to="/counsel">Chuyên gia tư vấn</Link>
+                        </li>
                     </div>
                     <div className="header__icon">
-                        <li onClick={() =>setIsOpenSearch(true)}>
+                        <li onClick={() => setIsOpenSearch(true)}>
                             <SearchOutlined />
                         </li>
                         <Link to="/cart">
-                        <li>
-                            <ShoppingCartOutlined />
-                        </li>
+                            <li>
+                                <ShoppingCartOutlined />
+                            </li>
                         </Link>
-                       
+
                         <li className="header__userdropdow">
                             <UserOutlined />
-                           <ul className="userdropdow">
-                            <Link to="/vieworder">
-                           <li>Xem đơn</li>
-                           </Link>
-                           <hr />
-                            <Link to="/profile">
-                            <li>Hồ sơ của tôi</li>
-                            </Link>
-                            <hr/>
-                            <Link to="/login">
-                            <li>Đăng xuất</li>
-                            </Link>
-                           </ul>
+                            <ul className="userdropdow">
+                                <Link to="/vieworder">
+                                    <li>Xem đơn</li>
+                                </Link>
+                                <hr />
+                                <Link to="/profile">
+                                    <li>Hồ sơ của tôi</li>
+                                </Link>
+                                <hr />
+                                <Link to="/login">
+                                    <li>Đăng xuất</li>
+                                </Link>
+                            </ul>
                         </li>
-                        
+
                     </div>
                 </ul>
             </nav>
